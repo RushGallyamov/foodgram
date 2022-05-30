49353cbaec837c5240f6ee8f5ee57c8110332605
RushGallyamov <78211479+RushGallyamov@users.noreply.github.com>
2022-05-30 13:41:22 +0500

    imports correction

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,7 +1,6 @@
 from django.contrib.auth import get_user_model
 from django.core import validators
 from django.db import models
-
 from colorfield.fields import ColorField
 
 User = get_user_model()
--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,11 +1,9 @@
 from django.shortcuts import get_object_or_404
-
 from drf_extra_fields.fields import Base64ImageField
 from rest_framework import serializers
 
 from users.models import Follow
 from users.serializers import CustomUserSerializer
-
 from .models import Ingredient, IngredientAmount, Recipe, Tag
 
 
